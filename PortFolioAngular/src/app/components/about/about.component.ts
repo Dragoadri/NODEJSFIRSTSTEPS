32f0a55cc7d11f95d828d120ffa6712e6fe72ced
Adrian <dragoadri@gmail.com>
2021-10-18 13:06:18 +0200

    comienzoPr

--- PortFolioAngular/src/app/components/about/about.component.ts
+++ PortFolioAngular/src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title:string;
+  public subtitle:string;
+  public email:string;
+  constructor() { 
+this.title="DragoDev";
+this.subtitle="FullStack";
+this.email="dragoadri@gmail.com"
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
