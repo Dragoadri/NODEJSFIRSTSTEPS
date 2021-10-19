43cf614ecc10115fff651b9e461b231a77f866e1
Adrian <dragoadri@gmail.com>
2021-10-19 14:49:59 +0200

    form

--- PortFolioAngular/src/app/services/project.service.ts
+++ PortFolioAngular/src/app/services/project.service.ts
@@ -2,17 +2,26 @@ import { Injectable } from "@angular/core";
 import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Project } from "../models/project";
-import { Global } from "./global";
 
 @Injectable()
+
     export class ProjectService{
+
         public url:string;
         constructor(
             private _http: HttpClient
         ){
-            this.url= Global.url;
+            this.url= global.url;
         }
         testService(){
             return 'Probando el servicio de angular'
         }
+        saveProject(project:Project):Observable<any>{
+            let params= JSON.stringify(project);
+            let headers = new HttpHeaders().set('Content-Type','application/json');
+            return this._http.post(this.url+'save-projects',params,{headers: headers});
+        }
+    }
+    var global ={
+        url: 'http://localhost:4200/api'
     }
